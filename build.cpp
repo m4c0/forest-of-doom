7a77a1e0b2361ac969d46c6924902f3dcffe27f7
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-05 16:36:18 +0100

    build: clean up deps with auto-discover

--- build.cpp
+++ build.cpp
@@ -1,38 +1,21 @@
-#include "../casein/build.hpp"
 #include "../ecow/ecow.hpp"
-#include "../hai/build.hpp"
-#include "../jute/build.hpp"
-#include "../missingno/build.hpp"
-#include "../pog/build.hpp"
 #include "../quack/build.hpp"
 #include "../rng/build.hpp"
-#include "../silog/build.hpp"
 #include "../sitime/build.hpp"
-#include "../stubby/build.hpp"
-#include "../traits/build.hpp"
-#include "../yoyo/build.hpp"
 
 using namespace ecow;
 
 auto base_app(const char *name) {
   auto a = unit::create<app>(name);
-  a->add_wsdep("casein", casein());
-  a->add_wsdep("hai", hai());
-  a->add_wsdep("jute", jute());
-  a->add_wsdep("missingno", missingno());
-  a->add_wsdep("pog", pog());
-  a->add_wsdep("rng", rng());
-  a->add_wsdep("quack", quack());
-  a->add_wsdep("silog", silog());
-  a->add_wsdep("sitime", sitime());
-  a->add_wsdep("stubby", stubby());
-  a->add_wsdep("traits", traits());
-  a->add_wsdep("yoyo", yoyo_libc());
   a->add_resource("1_Terrains_and_Fences_16x16.png");
   a->add_resource("11_Camping_16x16.png");
   a->add_resource("Modern_Exteriors_Characters_Scout_16x16_1.png");
   a->add_resource("Modern_UI_Style_1.png");
-  a->add_unit<box>(name);
+
+  auto b = a->add_unit<box>(name);
+  b->add_wsdep("quack", quack());
+  b->add_wsdep("rng", rng());
+  b->add_wsdep("sitime", sitime());
   return a;
 }
 
