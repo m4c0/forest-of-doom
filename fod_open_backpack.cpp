ad6b8777b1416988e640e3c10730d1d115f80b7d
Eduardo Costa <m4c0@users.noreply.github.com>
2025-07-17 20:02:17 +0100

    drawing arbitrary boxes

--- fod_open_backpack.cpp
+++ fod_open_backpack.cpp
@@ -1,67 +1,31 @@
 module fod;
 import backpack;
+import dotz;
 import fox;
 import input;
 import player;
 
 static void on_frame(float ms) {
   fox::g->load_ui([](auto * m) {
-    *m += {
-      .pos { 0, 0 },
-      .uv { 3, 8 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
+    const auto sp = [&](dotz::vec2 pos, dotz::vec2 uv) {
+      *m += { .pos = pos, .uv = uv, .size  = 1, .texid = fox::texids::ui_style };
     };
-    *m += {
-      .pos { 1, 0 },
-      .uv { 4, 8 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
-    };
-    *m += {
-      .pos { 2, 0 },
-      .uv { 5, 8 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
-    };
+    const auto box = [&](dotz::vec2 pos, dotz::vec2 size) {
+      sp({ 0, 0 }, { 3, 8 });
+      for (auto x = 0; x < size.x; x++) sp({ x + 1, 0 }, { 4, 8 });
+      sp({ size.x + 1, 0.f }, { 5, 8 });
 
-    *m += {
-      .pos { 0, 1 },
-      .uv { 0, 11 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
-    };
-    *m += {
-      .pos { 1, 1 },
-      .uv { 1, 11 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
-    };
-    *m += {
-      .pos { 2, 1 },
-      .uv { 2, 11 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
-    };
+      for (auto y = 0; y < size.y; y++) {
+        sp({ 0, y + 1 }, { 0, 11 });
+        for (auto x = 0; x < size.x; x++) sp({ x + 1, y + 1 }, { 1, 11 });
+        sp({ size.x + 1, y + 1.f }, { 2, 11 });
+      }
 
-    *m += {
-      .pos { 0, 2 },
-      .uv { 3, 9 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
+      sp({ 0.0f, size.y + 1 }, { 3, 9 });
+      for (auto x = 0; x < size.x; x++) sp({ x + 1.f, size.y + 1 }, { 4, 9 });
+      sp({ size.x + 1, size.y + 1 }, { 5, 9 });
     };
-    *m += {
-      .pos { 1, 2 },
-      .uv { 4, 9 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
-    };
-    *m += {
-      .pos { 2, 2 },
-      .uv { 5, 9 },
-      .size = 1,
-      .texid = fox::texids::ui_style,
-    };
+    box({ 0, 0 }, { 5, 3 });
   });
   fox::g->on_frame(16, 16, player::center());
 }
