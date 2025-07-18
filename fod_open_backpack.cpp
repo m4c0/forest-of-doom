e2a51d24ff4ef4374f8e3ee529d0373eec7afe47
Eduardo Costa <m4c0@users.noreply.github.com>
2025-07-18 17:52:16 +0100

    backpack: drawing using the new sprites

--- fod_open_backpack.cpp
+++ fod_open_backpack.cpp
@@ -5,39 +5,43 @@ import fox;
 import input;
 import player;
 
+static dotz::ivec2 g_cursor {};
+
 static void on_frame(float ms) {
   fox::g->load_ui([](auto * m) {
-    const dotz::vec2 size { 10, 9 };
+    const dotz::vec2 size { 8 };
     const dotz::vec2 tl = -(size + 3) / 2.0;
+    const dotz::vec2 gtl = 0.3f;
+    const dotz::vec2 csz = 1.2f;
 
     const auto sp = [&](dotz::vec2 pos, dotz::vec2 uv, dotz::vec2 size = 1) {
-      *m += { .pos = tl + pos, .uv = uv, .size = size, .texid = fox::texids::ui_style };
+      *m += { .pos = tl + pos, .uv = uv, .size = size, .texid = fox::texids::ui_paper };
     };
     const auto box = [&] {
-      sp({ 0, 0 }, { 3, 8 });
-      for (auto x = 0; x < size.x; x++) sp({ x + 1, 0 }, { 4, 8 });
-      sp({ size.x + 1, 0.f }, { 5, 8 });
+      sp({ 0, 0 }, { 1, 1 });
+      for (auto x = 0; x < size.x; x++) sp({ x + 1, 0 }, { 2, 1 });
+      sp({ size.x + 1, 0.f }, { 3, 1 });
 
       for (auto y = 0; y < size.y; y++) {
-        sp({ 0, y + 1 }, { 0, 11 });
-        for (auto x = 0; x < size.x; x++) sp({ x + 1, y + 1 }, { 1, 11 });
-        sp({ size.x + 1, y + 1.f }, { 2, 11 });
+        sp({ 0, y + 1 }, { 1, 2 });
+        for (auto x = 0; x < size.x; x++) sp({ x + 1, y + 1 }, { 2, 2 });
+        sp({ size.x + 1, y + 1.f }, { 3, 2 });
       }
 
-      sp({ 0.0f, size.y + 1 }, { 3, 9 });
-      for (auto x = 0; x < size.x; x++) sp({ x + 1.f, size.y + 1 }, { 4, 9 });
-      sp({ size.x + 1, size.y + 1 }, { 5, 9 });
+      sp({ 0.0f, size.y + 1 }, { 1, 3 });
+      for (auto x = 0; x < size.x; x++) sp({ x + 1.f, size.y + 1 }, { 2, 3 });
+      sp({ size.x + 1, size.y + 1 }, { 3, 3 });
     };
 
     box();
 
-    for (auto y = 0; y < 8; y++) {
-      for (auto x = 0; x < 8; x++) {
-        sp({ x * 1.2f + 0.3f, y * 1.2f - 0.2f }, { 0, 6 }, 3);
+    for (dotz::ivec2 p = 0; p.y < 8; p.y++) {
+      for (p.x = 0; p.x < 8; p.x++) {
+        sp(p * csz + gtl, { 5, 5 });
       }
     }
 
-    sp({ 1.0f + 0.3f, 1.0f - 0.2f }, { 8, 0 });
+    sp(g_cursor * csz + gtl, { 15, 4 });
   });
   fox::g->on_frame(16, 16, player::center());
 }
@@ -47,4 +51,5 @@ void fod::open_backpack() {
   input::on_button_down(input::buttons::CANCEL, fod::poc);
   input::on_button_down(input::buttons::REST, [] {});
   fod::on_frame = ::on_frame;
+  g_cursor = {};
 }
