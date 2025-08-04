ed55e36e7bd18ad5526d3b8c21fa29634fc70ee4
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-04 14:10:21 +0100

    loots: item function on lispy

--- loots_lispy.cpp
+++ loots_lispy.cpp
@@ -3,8 +3,12 @@ import lispy;
 
 using namespace lispy;
 
+struct loot_node : node {
+  loots::item item;
+};
+
 hai::array<loots::item> loots::load(jute::view table_name) {
-  hai::array<node> buf { 10240 };
+  hai::array<loot_node> buf { 10240 };
   auto cur = buf.begin();
   alloc_node = [&] -> void * {
     if (cur == buf.end()) throw 0;
@@ -12,6 +16,15 @@ hai::array<loots::item> loots::load(jute::view table_name) {
   };
 
   context ctx {};
+  ctx.fns["item"] = [](auto ctx, auto n, auto aa, auto as) -> const node * {
+    if (as != 2) err(n, "item expects two coordinates");
+
+    auto * nn = new loot_node { *n };
+    nn->item.sprite.x = to_i(aa[0]);
+    nn->item.sprite.y = to_i(aa[1]);
+    return nn;
+  };
+
   hai::array<loots::item> res { 8 };
   run(table_name, ctx, [&](auto * node) {
   });
