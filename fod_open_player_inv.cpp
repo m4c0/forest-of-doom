e038901cdd0f537f25dbc63a72d7f69df77b1a48
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-07 18:35:43 +0100

    op: showing player inventory

--- fod_open_player_inv.cpp
+++ fod_open_player_inv.cpp
@@ -1,13 +1,20 @@
 module fod;
 import fox;
+import fui;
+import input;
 import player;
 
 static void on_frame(float ms) {
   fox::g->load_ui([](auto * m) {
+    fui::inv { &player::inv::inv(), {}, { -1 } }.load(m, {});
   });
   fox::g->on_frame(16, 16, player::center());
 }
 
 void fod::open_player_inv() {
   fod::on_frame = ::on_frame;
+
+  using namespace input;
+  reset();
+  on_key_down(keys::CANCEL, fod::poc); // TODO: discard selection or quit
 }
