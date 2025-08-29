61d131a8664c1d4f917f7b86eabd6f772e53bc2e
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-29 18:30:49 +0100

    pathing: parsing entry/file

--- pathing_checker.cpp
+++ pathing_checker.cpp
@@ -6,15 +6,38 @@ import print;
 
 int main() try {
   struct node : lispy::node {
+    jute::heap file;
+    jute::heap enter;
     jute::heap start;
   };
 
   lispy::ctx_w_mem<node> cm {};
+  cm.ctx.fns["enter"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
+    if (as != 1) lispy::err(n, "enter expects entry name");
+    auto a = static_cast<const node *>(lispy::eval(ctx, aa[0]));
+    if (!lispy::is_atom(a)) lispy::err(aa[0], "expecting atom as entry name");
+
+    auto nn = new (ctx.allocator()) node {};
+    nn->enter = a->atom;
+    return nn;
+  };
+  cm.ctx.fns["file"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
+    if (as != 1) lispy::err(n, "file expects prefab name");
+    auto a = static_cast<const node *>(lispy::eval(ctx, aa[0]));
+    if (!lispy::is_atom(a)) lispy::err(aa[0], "expecting atom as file name");
+
+    auto nn = new (ctx.allocator()) node {};
+    nn->file = a->atom;
+    return nn;
+  };
   cm.ctx.fns["from"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
     if (as < 1) lispy::err(n, "from expects file, enter and one or more exit");
-
-    auto nn = new (ctx.allocator()) node {
-    };
+    auto nn = new (ctx.allocator()) node {};
+    for (auto i = 0; i < as; i++) {
+      auto a = static_cast<const node *>(lispy::eval(ctx, aa[i]));
+      if (*a->file != "") nn->file = a->file;
+      if (*a->enter != "") nn->enter = a->enter;
+    }
     return nn;
   };
   cm.ctx.fns["start"] = [](auto ctx, auto n, auto aa, auto as) -> const lispy::node * {
